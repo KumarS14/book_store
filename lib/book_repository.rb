78c05d564a0e8f6d02cc0e71230127f98e0e3de8
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-01 19:17:24 +0000

    require added

--- lib/book_repository.rb
+++ lib/book_repository.rb
@@ -1,12 +1,14 @@
+require 'book'
+
 class BookRepository
     def all
-        result_set = DatabaseConnection.exec_params('SELECT id, title, author_name FROM books;' [] )
+        result_set = DatabaseConnection.exec_params('SELECT id, title, author_name FROM books;', [] )
         books = []
         result_set.each do |a_book|
             book = Book.new
             book.id = a_book['id']
             book.title = a_book['title']
-            book.author_name = a_book["author_name"]
+            book.author_name = a_book['author_name']
             books << book
 
         end
