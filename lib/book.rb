f31d2cb1b0d842b10d73f55a3e15eb6cead01079
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-01 18:55:44 +0000

    creation of bookstore class

---
+++ lib/book.rb
@@ -0,0 +1,16 @@
+# EXAMPLE
+# Table name: books
+
+# Model class
+# (in lib/student.rb)
+
+class Bookstore
+
+    # Replace the attributes by your own columns.
+    attr_accessor :id, :title, :author_name
+  end
+  
+  # The keyword attr_accessor is a special Ruby feature
+  # which allows us to set and get attributes on an object,
+  # here's an example:
+  #
